d3a82f41ffbd321850c31d468a630a2b0ceb6aa9
Darya Rozhnova <hxrvelle@gmail.com>
2024-01-16 16:00:25 +0300

    --kafka-elk

--- src/main/java/com/example/RxJava/service/kafka_comsumer/KafkaConsumer.java
+++ src/main/java/com/example/RxJava/service/kafka_comsumer/KafkaConsumer.java
@@ -4,16 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Service
-@Slf4j
+//@Service
+//@Slf4j
 public class KafkaConsumer {
-    @KafkaListener(topics = "users-success", groupId = "user-group")
-    public void listenUsersSuccess(String message) {
-        log.info("Received Success Message in group 'user-group': " + message);
-    }
-
-    @KafkaListener(topics = "users-error", groupId = "user-group")
-    public void listenUsersError(String message) {
-        log.error("Received Error Message in group 'user-group': " + message);
-    }
+//    @KafkaListener(topics = "users-success", groupId = "user-group")
+//    public void listenUsersSuccess(String message) {
+//        log.info("Received Success Message in group 'user-group': " + message);
+//    }
+//
+//    @KafkaListener(topics = "users-error", groupId = "user-group")
+//    public void listenUsersError(String message) {
+//        log.error("Received Error Message in group 'user-group': " + message);
+//    }
 }
